e9511be6a89c26d9ce75669356017d8a06263546
ahmedalaa11 <47926165+ahmedalaa11@users.noreply.github.com>
2019-10-15 21:32:43 +0200

    Update MIPS_ALU.v

--- MIPS_ALU.v
+++ MIPS_ALU.v
@@ -104,19 +104,4 @@ ALUcontol c1 (fn, op, ctl);
 endmodule
 
 
-module clock(clk);
-`timescale 100ps/1ps
-output reg clk;
-initial
-	begin
-		assign clk = 0;
-	end
-always
-	begin
-		#625
-		assign clk = 1;
-		#625
-		assign clk = 0;
-	end
-endmodule
 
