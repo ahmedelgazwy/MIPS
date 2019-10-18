b55f31fa1111bae69539b25bacbb49786bcf9198
ahmedelgazwy <56591917+ahmedelgazwy@users.noreply.github.com>
2019-10-18 22:45:42 +0200

    Update PC.v

--- PC.v
+++ PC.v
@@ -3,7 +3,14 @@ module PC(Out,In,clk);
 input clk;
 input [31:0]In;
 output reg [31:0]Out;
-
+/*initial
+  begin
+  intger i;
+  for(i=0;i<31;i++)
+  begin
+  pc[i]=0;
+  end
+  end*/
 always@(posedge clk)
 begin
 
